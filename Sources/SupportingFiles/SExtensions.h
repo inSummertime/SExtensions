f160be9b539c4cc7420a8adc2c00c769db3c6fe9
Ray <therayfeng@gmail.com>
2018-05-26 13:29:13 +0800

    Relocate to SupportingFiles folder

---
+++ Sources/SupportingFiles/SExtensions.h
@@ -0,0 +1,19 @@
+//
+//  SExtensions.h
+//  SExtensions
+//
+//  Created by Ray on 2018/5/26.
+//  Copyright © 2018年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SExtensions.
+FOUNDATION_EXPORT double SExtensionsVersionNumber;
+
+//! Project version string for SExtensions.
+FOUNDATION_EXPORT const unsigned char SExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SExtensions/PublicHeader.h>
+
+
