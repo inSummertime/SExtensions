d41cbdcdef36f7ceadfba0bbae1b96caa025b55c
Ray <therayfeng@gmail.com>
2018-06-07 10:06:09 +0800

    NSAttributedString+Concatenate

---
+++ Sources/Foundation/String/NSAttributedString+Concatenate.swift
@@ -0,0 +1,31 @@
+//
+//  NSAttributedString+Concatenate.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/6/7.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+extension NSAttributedString {
+    
+    /// Returns the concatenation of lhs and rhs
+    ///
+    ///     let hello = NSAttributedString(string: "Hello")
+    ///     let world = NSAttributedString(string: " world!")
+    ///     print((hello + world).string)
+    ///     // Prints "Hello world!"
+    ///
+    /// - Parameters:
+    ///   - lhs: a NSAttributedString
+    ///   - rhs: a NSAttributedString
+    /// - Returns: a concatenated NSAttributedString
+    static func + (lhs: NSAttributedString, rhs: NSAttributedString) -> NSAttributedString {
+        let result = NSMutableAttributedString()
+        result.append(lhs)
+        result.append(rhs)
+        return NSAttributedString(attributedString: result)
+    }
+    
+}
