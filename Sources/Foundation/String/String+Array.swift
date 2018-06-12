e6664cbf24b91957e48ffaf428a8f418d082ee3c
Ray <therayfeng@gmail.com>
2018-06-13 07:44:48 +0800

    String+Array

---
+++ Sources/Foundation/String/String+Array.swift
@@ -0,0 +1,36 @@
+//
+//  String+Array.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/6/13.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    /// An array if possible
+    ///
+    ///     print("[1, \"Hello world!\"]".array)
+    ///     // Prints "Optional([1, Hello world!])"
+    var array: [Any]? {
+        if let data = data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+    
+    /// An array or an empty array
+    ///
+    ///     print("[1, \"Hello world!\"]".arrayValue[0])
+    ///     // Prints "1"
+    var arrayValue: [Any] {
+        return array ?? [Any]()
+    }
+    
+}
