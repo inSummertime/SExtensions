a350cc7b3868b640766330b8e4062607b5a879aa
Ray <therayfeng@gmail.com>
2018-07-08 07:38:49 +0800

    Double+Percentage

---
+++ Sources/Foundation/Double/Double+Percentage.swift
@@ -0,0 +1,28 @@
+//
+//  Double+Percentage.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/7/8.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Double {
+    
+    /// Returns a optional percentage string
+    ///
+    ///     print(0.55555.percentage(maximumFractionDigits: 1))
+    ///     // Prints "Optional("55.6%")"
+    ///
+    /// - Parameter maximumFractionDigits: The maximum number of digits after
+    ///   the decimal separator.
+    /// - Returns: a optional percentage string
+    func percentage(maximumFractionDigits: Int = 0) -> String? {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .percent
+        formatter.maximumFractionDigits = maximumFractionDigits
+        return formatter.string(from: NSNumber(value: self))
+    }
+    
+}
