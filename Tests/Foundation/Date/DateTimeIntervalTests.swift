f97b8a9e092fe6c2f31888d5a6ed54d78e654470
Ray <therayfeng@gmail.com>
2018-07-14 08:01:38 +0800

    DateTimeIntervalTests

---
+++ Tests/Foundation/Date/DateTimeIntervalTests.swift
@@ -0,0 +1,20 @@
+//
+//  DateTimeIntervalTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 7/14/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class DateTimeIntervalTests: XCTestCase {
+    
+    func testTimeInterval() {
+        XCTAssertEqual(Date.timeIntervalPerWeek / 7.0, Date.timeIntervalPerDay)
+        XCTAssertEqual(Date.timeIntervalPerDay / 24.0, Date.timeIntervalPerHour)
+        XCTAssertEqual(Date.timeIntervalPerHour / 60.0, Date.timeIntervalPerMinute)
+    }
+    
+}
