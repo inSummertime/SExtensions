162d65d693992a078d64091790e1ef865cd316a3
Ray <therayfeng@gmail.com>
2018-07-14 08:01:29 +0800

    Date+TimeInterval

---
+++ Sources/Foundation/Date/Date+TimeInterval.swift
@@ -0,0 +1,25 @@
+//
+//  Date+TimeInterval.swift
+//  SExtensions
+//
+//  Created by Ray on 7/14/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    /// Number of seconds per minute
+    static let timeIntervalPerMinute: TimeInterval = 60.0
+    
+    /// Number of seconds per hour
+    static let timeIntervalPerHour: TimeInterval = 3600.0
+    
+    /// Number of seconds per day
+    static let timeIntervalPerDay: TimeInterval = 86400.0
+    
+    /// Number of seconds per week
+    static let timeIntervalPerWeek: TimeInterval = 604800.0
+    
+}
