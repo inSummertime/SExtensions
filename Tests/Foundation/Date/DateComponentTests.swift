61f56896c5d3ddb149d9f98aa6f0541a3b96c3f6
Ray <therayfeng@gmail.com>
2018-07-23 23:13:10 +0800

    testComponents update

--- Tests/Foundation/Date/DateComponentTests.swift
+++ Tests/Foundation/Date/DateComponentTests.swift
@@ -13,9 +13,9 @@ final class DateComponentTests: XCTestCase {
     
     func testComponents() {
         let date = Date(timeIntervalSinceReferenceDate: 0)
-        XCTAssertEqual(date.year, 2001)
-        XCTAssertEqual(date.month, 01)
-        XCTAssertEqual(date.day, 01)
+        XCTAssertTrue(date.year == 2001 || date.year == 2000)
+        XCTAssertTrue(date.month == 1 || date.month == 12)
+        XCTAssertTrue(date.day == 01 || date.day == 31)
         XCTAssertEqual(date.minute, 0)
         XCTAssertEqual(date.second, 0)
     }
