ce011ea62db2d80cacf95bd19874047d26fc59ba
Ray <therayfeng@gmail.com>
2018-08-01 06:52:44 +0800

    DateComponentTests updates

--- Tests/Foundation/Date/DateComponentTests.swift
+++ Tests/Foundation/Date/DateComponentTests.swift
@@ -24,12 +24,19 @@ final class DateComponentTests: XCTestCase {
         let date = Date(timeIntervalSinceReferenceDate: 0)
         var calendar = Calendar(identifier: Calendar.Identifier.gregorian)
         calendar.timeZone = TimeZone(secondsFromGMT: 0)!
-        XCTAssertEqual(date.component(.year, in: calendar), 2001)
-        XCTAssertEqual(date.component(.month, in: calendar), 01)
-        XCTAssertEqual(date.component(.day, in: calendar), 01)
-        XCTAssertEqual(date.component(.hour, in: calendar), 0)
-        XCTAssertEqual(date.component(.minute, in: calendar), 0)
-        XCTAssertEqual(date.component(.second, in: calendar), 0)
+        XCTAssertEqual(date.component(.year, in: calendar)!, 2001)
+        XCTAssertEqual(date.component(.month, in: calendar)!, 01)
+        XCTAssertEqual(date.component(.day, in: calendar)!, 01)
+        XCTAssertEqual(date.component(.hour, in: calendar)!, 0)
+        XCTAssertEqual(date.component(.minute, in: calendar)!, 0)
+        XCTAssertEqual(date.component(.second, in: calendar)!, 0)
+    }
+    
+    func testSubscript() {
+        let date = Date(timeIntervalSinceReferenceDate: 0)
+        XCTAssertEqual(date[.year]!, 2001)
+        XCTAssertEqual(date[.month]!, 01)
+        XCTAssertEqual(date[.day]!, 01)
     }
     
     func testComponentSinceDateInCalendar() {
