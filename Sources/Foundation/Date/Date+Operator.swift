0750b2a1b882470839b8261411fb4b1622ff8b68
Ray <therayfeng@gmail.com>
2018-08-05 06:59:59 +0800

    Date+Operator update

--- Sources/Foundation/Date/Date+Operator.swift
+++ Sources/Foundation/Date/Date+Operator.swift
@@ -25,12 +25,36 @@ public extension Date {
         return lhs.timeIntervalSince(rhs)
     }
     
-    /// Returns true if the left hand `Date` is earlier in time than the right hand `Date` or the two `Date` values represent the same point in time.
+    /// Returns true if the left hand `Date` is earlier in time than the
+    /// right hand `Date` or the two `Date` values represent the same point
+    /// in time.
+    ///
+    ///     let date = Date(timeIntervalSinceReferenceDate: 0)
+    ///     let next = date.addingTimeInterval(1)
+    ///     print(date <= next)
+    ///     // Prints "true"
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    /// - Returns: a Boolean value.
     static func <= (lhs: Date, rhs: Date) -> Bool {
         return lhs == rhs || lhs < rhs
     }
     
-    /// Returns true if the left hand `Date` is later in time than the right hand `Date` or the two `Date` values represent the same point in time.
+    /// Returns true if the left hand `Date` is later in time than the right
+    /// hand `Date` or the two `Date` values represent the same point in
+    /// time.
+    ///
+    ///     let date = Date(timeIntervalSinceReferenceDate: 0)
+    ///     let next = date.addingTimeInterval(1)
+    ///     print(next >= date)
+    ///     // Prints "true"
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    /// - Returns: a Boolean value.
     static func >= (lhs: Date, rhs: Date) -> Bool {
         return lhs == rhs || lhs > rhs
     }
