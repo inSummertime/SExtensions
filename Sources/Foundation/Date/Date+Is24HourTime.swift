dd13b48f0c66c19d8d5156bddcfa2769e41a2bb9
Ray <therayfeng@gmail.com>
2018-08-04 11:32:38 +0800

    Date+Is24HourTime

---
+++ Sources/Foundation/Date/Date+Is24HourTime.swift
@@ -0,0 +1,23 @@
+//
+//  Date+Is24HourTime.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/4.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    
+    /// whether is 24-hour time
+    var is24HourTime: Bool {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        let dateString = formatter.string(from: self)
+        let amRange = dateString.range(of: formatter.amSymbol)
+        let pmRange = dateString.range(of: formatter.pmSymbol)
+        return amRange == nil && pmRange == nil
+    }
+    
+}
