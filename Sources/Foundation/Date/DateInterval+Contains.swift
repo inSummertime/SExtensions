1fab26875be84d7f061d821e77e2f22692e44948
Ray <therayfeng@gmail.com>
2018-08-10 07:50:36 +0800

    DateInterval+Contains

---
+++ Sources/Foundation/Date/DateInterval+Contains.swift
@@ -0,0 +1,26 @@
+//
+//  DateInterval+Contains.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/10.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension DateInterval {
+    
+    /// Returns `true` if `self` contains another `dateInterval`.
+    ///
+    ///     let dateInterval = DateInterval(start: Date(timeIntervalSinceReferenceDate: 0), duration: 2.0)
+    ///     print(dateInterval.contains(DateInterval(start: Date(timeIntervalSinceReferenceDate: 0), duration: 1.0)))
+    ///     // Prints "true"
+    ///
+    /// - Parameter dateInterval: A `dateInterval`.
+    /// - Returns: Returns `true` if `self` contains another
+    ///   `dateInterval`.
+    func contains(_ dateInterval: DateInterval) -> Bool {
+        return dateInterval.start >= start && dateInterval.end <= end
+    }
+    
+}
