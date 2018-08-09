5adff611b6b8da54741348f432f9e68ffb3355bc
Ray <therayfeng@gmail.com>
2018-08-10 07:50:50 +0800

    DateIntervalContainsTests

---
+++ Tests/Foundation/Date/DateIntervalContainsTests.swift
@@ -0,0 +1,22 @@
+//
+//  DateIntervalContainsTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/10.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class DateIntervalContainsTests: XCTestCase {
+    
+    func testContains() {
+        let dateInterval = DateInterval(start: Date(timeIntervalSinceReferenceDate: 0), duration: 2.0)
+        XCTAssertTrue(dateInterval.contains(dateInterval))
+        XCTAssertTrue(dateInterval.contains(DateInterval(start: Date(timeIntervalSinceReferenceDate: 0), duration: 1.0)))
+        XCTAssertTrue(dateInterval.contains(DateInterval(start: Date(timeIntervalSinceReferenceDate: 0).addingTimeInterval(1.0), duration: 1.0)))
+        XCTAssertFalse(dateInterval.contains(DateInterval(start: Date(timeIntervalSinceReferenceDate: 0).addingTimeInterval(2.0), duration: 1.0)))
+    }
+    
+}
