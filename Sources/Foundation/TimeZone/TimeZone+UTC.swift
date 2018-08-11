10c2d97321a935ddce65fd046a40ac23705cafc5
Ray <therayfeng@gmail.com>
2018-08-12 07:43:58 +0800

    TimeZone+UTC

---
+++ Sources/Foundation/TimeZone/TimeZone+UTC.swift
@@ -0,0 +1,17 @@
+//
+//  TimeZone+UTC.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/12.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension TimeZone {
+    
+    static var UTC: TimeZone {
+        return TimeZone(identifier: "UTC")!
+    }
+    
+}
