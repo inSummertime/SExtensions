b6c07e14d076d009f79ca26aff43756f36634d1d
Ray <therayfeng@gmail.com>
2018-08-13 08:04:36 +0800

    TimeZoneUTCTests

---
+++ Tests/Foundation/TimeZone/TimeZoneUTCTests.swift
@@ -0,0 +1,18 @@
+//
+//  TimeZoneUTCTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/13.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class TimeZoneUTCTests: XCTestCase {
+    
+    func testUTC() {
+        XCTAssertEqual(TimeZone.UTC.identifier, "GMT")
+    }
+    
+}
