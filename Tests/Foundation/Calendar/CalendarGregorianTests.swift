ec849edf76151c52c67c06ecc55468de24ba1c88
Ray <therayfeng@gmail.com>
2018-08-15 08:29:17 +0800

    CalendarGregorianTests

---
+++ Tests/Foundation/Calendar/CalendarGregorianTests.swift
@@ -0,0 +1,18 @@
+//
+//  CalendarGregorianTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/15.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CalendarGregorianTests: XCTestCase {
+    
+    func testGregorian() {
+        XCTAssertEqual(Calendar.gregorian.identifier, Calendar.Identifier.gregorian)
+    }
+    
+}
