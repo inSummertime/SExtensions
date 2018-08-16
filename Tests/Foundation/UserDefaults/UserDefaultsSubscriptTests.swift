9f8c26031236050e12e28c250f74ffa68521d98e
Ray <therayfeng@gmail.com>
2018-08-16 08:28:33 +0800

    UserDefaultsSubscriptTests

---
+++ Tests/Foundation/UserDefaults/UserDefaultsSubscriptTests.swift
@@ -0,0 +1,26 @@
+//
+//  UserDefaultsSubscriptTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/16.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class UserDefaultsSubscriptTests: XCTestCase {
+    
+    func testSubscript() {
+        let key = "HelloWorld!"
+        XCTAssertNil(UserDefaults.standard[key])
+        UserDefaults.standard.set(true, forKey: key)
+        XCTAssertTrue(UserDefaults.standard[key]! as! Bool)
+        print(UserDefaults.standard[key])
+        UserDefaults.standard.set(false, forKey: key)
+        XCTAssertFalse(UserDefaults.standard[key]! as! Bool)
+        UserDefaults.standard.removeObject(forKey: key)
+        XCTAssertNil(UserDefaults.standard[key])
+    }
+    
+}
