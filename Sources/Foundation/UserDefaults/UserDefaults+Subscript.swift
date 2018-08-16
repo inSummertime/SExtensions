db3c7c63cb89cc8d9af6984f5a7bb13d3d3500f2
Ray <therayfeng@gmail.com>
2018-08-16 08:28:23 +0800

    UserDefaults+Subscript

---
+++ Sources/Foundation/UserDefaults/UserDefaults+Subscript.swift
@@ -0,0 +1,30 @@
+//
+//  UserDefaults+Subscript.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/16.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension UserDefaults {
+    
+    /// Searches the receiver's search list for a default with the
+    /// key and return it using subscript.
+    ///
+    ///     UserDefaults.standard.set(true, forKey: "HelloWorld!")
+    ///     print(UserDefaults.standard["HelloWorld!"])
+    ///     // Prints "Optional(1)"
+    ///
+    /// - Parameter key: The key to search with.
+    public subscript(key: String) -> Any? {
+        get {
+            return object(forKey: key)
+        }
+        set {
+            set(newValue, forKey: key)
+        }
+    }
+    
+}
