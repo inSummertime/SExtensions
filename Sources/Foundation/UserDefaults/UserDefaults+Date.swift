aeab15568dad3b90c6c6f53013381bf5f4e35c26
Ray <therayfeng@gmail.com>
2018-08-17 21:31:14 +0800

    UserDefaults+Date

---
+++ Sources/Foundation/UserDefaults/UserDefaults+Date.swift
@@ -0,0 +1,27 @@
+//
+//  UserDefaults+Date.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/17.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension UserDefaults {
+    
+    /// Searches the receiver's search list for a default with
+    /// the key 'defaultName' and converts the returned value to a
+    /// Date and returns nil if the value is not a Date.
+    ///
+    ///     UserDefaults.standard.set(Date(timeIntervalSinceReferenceDate: 0), forKey: "HelloWorld!")
+    ///     print(UserDefaults.standard.date(forKey: "HelloWorld!")! == Date(timeIntervalSinceReferenceDate: 0))
+    ///     // Prints "true"
+    ///
+    /// - Parameter defaultName: The key to search with.
+    /// - Returns: A date or nil
+    func date(forKey defaultName: String) -> Date? {
+        return object(forKey: defaultName) as? Date
+    }
+    
+}
