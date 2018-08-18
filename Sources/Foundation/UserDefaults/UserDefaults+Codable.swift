5e76654e044d28a3de6897b2f702a1f4f1dbe008
Ray <therayfeng@gmail.com>
2018-08-18 09:24:14 +0800

    UserDefaults+Codable

---
+++ Sources/Foundation/UserDefaults/UserDefaults+Codable.swift
@@ -0,0 +1,41 @@
+//
+//  UserDefaults+Codable.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension UserDefaults {
+    
+    /// Searches the receiver's search list for a default with
+    /// the key 'defaultName' and converts the returned value to a
+    /// Codable object and returns nil if no value or the value is
+    /// not Codable.
+    ///
+    /// - Parameters:
+    ///   - type: A Codable type.
+    ///   - defaultName: The key to search with.
+    /// - Returns: A Decodable object or nil.
+    func codable<T: Codable>(_ type: T.Type, forKey defaultName: String) -> T? {
+        guard let data = object(forKey: defaultName) as? Data else { return nil }
+        return try? JSONDecoder().decode(type, from: data)
+    }
+    
+    /// Stores an Encodable value (or removes the value if nil is
+    /// passed as the value) for the provided key in the search list
+    /// entry for the receiver's suite name in the current user and
+    /// any host, then asynchronously stores the value persistently,
+    /// where it is made available to other processes.
+    ///
+    /// - Parameters:
+    ///   - value: The Encodable value to store.
+    ///   - defaultName: The provided key.
+    func setCodable<T: Codable>(_ value: T, forKey defaultName: String) {
+        let data = try? JSONEncoder().encode(value)
+        set(data, forKey: defaultName)
+    }
+    
+}
