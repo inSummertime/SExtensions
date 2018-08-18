bed9a7290c02e17a2399afa43b99b8a003239de8
Ray <therayfeng@gmail.com>
2018-08-18 09:24:28 +0800

    UserDefaultsCodableTests

---
+++ Tests/Foundation/UserDefaults/UserDefaultsCodableTests.swift
@@ -0,0 +1,25 @@
+//
+//  UserDefaultsCodableTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class UserDefaultsCodableTests: XCTestCase {
+    
+    func testCodable() {
+        struct Object: Codable {
+            let id: Int
+        }
+        let object = Object(id: 0)
+        let key = "HelloWorld!"
+        UserDefaults.standard.setCodable(object, forKey: "HelloWorld!")
+        XCTAssertEqual(UserDefaults.standard.codable(Object.self, forKey: key)!.id, 0)
+        UserDefaults.standard.removeObject(forKey: key)
+    }
+    
+}
