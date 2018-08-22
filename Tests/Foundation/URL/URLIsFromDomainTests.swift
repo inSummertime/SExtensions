0e5175330cfcf5a399def2621c4a5576fe711253
Ray <therayfeng@gmail.com>
2018-08-22 08:04:29 +0800

    URLIsFromDomainTests

---
+++ Tests/Foundation/URL/URLIsFromDomainTests.swift
@@ -0,0 +1,24 @@
+//
+//  URLIsFromDomainTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/22.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class URLIsFromDomainTests: XCTestCase {
+    
+    func testIsFromDomain() {
+        let baseURL = URL(string: "https://www.google.com")
+        let url = URL(string: "search?q=iPhone", relativeTo: baseURL)!
+        XCTAssertTrue(url.isFromDomain("google.com"))
+        XCTAssertTrue(url.isFromDomain("www.google.com"))
+        let absoluteURL = URL(string: "https://www.google.com/search?q=iPhone")!
+        XCTAssertTrue(absoluteURL.isFromDomain("google.com"))
+        XCTAssertTrue(absoluteURL.isFromDomain("www.google.com"))
+    }
+    
+}
