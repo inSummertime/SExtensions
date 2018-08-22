d33d5a022b9999f4eb4eb58d19b6939b8b7d2e7f
Ray <therayfeng@gmail.com>
2018-08-22 08:04:15 +0800

    URL+IsFromDomain

---
+++ Sources/Foundation/URL/URL+IsFromDomain.swift
@@ -0,0 +1,43 @@
+//
+//  URL+IsFromDomain.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/22.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension URL {
+    
+    /// Returns true if the url is from a domain.
+    ///
+    ///     let url = URL(string: "https://www.google.com/search?q=iPhone")!
+    ///     print(url.isFromDomain("google.com"))
+    ///     // Prints "true"
+    ///     print(url.isFromDomain("www.google.com"))
+    ///     // Prints "true"
+    ///
+    /// - Parameter domain: The domain.
+    /// - Returns: Returns true if the url is from a domain.
+    func isFromDomain(_ domain: String) -> Bool {
+        guard let host = host else {
+            return false
+        }
+        var selfHostComponents = host.components(separatedBy: ".")
+        var targetHostComponents = domain.components(separatedBy: ".")
+        let selfComponentsCount = selfHostComponents.count
+        let targetComponentsCount = targetHostComponents.count
+        let offset = selfComponentsCount - targetComponentsCount
+        if offset < 0 {
+            return false
+        }
+        for i in offset..<selfComponentsCount {
+            if !(selfHostComponents[i] == targetHostComponents[i - offset]) {
+                return false
+            }
+        }
+        return true
+    }
+    
+}
