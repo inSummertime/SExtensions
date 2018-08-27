2aac3ed66600a474cf194c56eb09f3e905fc94b9
Ray <therayfeng@gmail.com>
2018-08-27 20:23:32 +0800

    URLRequestSetHTTPHeaderFieldsTests

---
+++ Tests/Foundation/URLRequest/URLRequestSetHTTPHeaderFieldsTests.swift
@@ -0,0 +1,23 @@
+//
+//  URLRequestSetHTTPHeaderFieldsTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/8/26.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class URLRequestSetHTTPHeaderFieldsTests: XCTestCase {
+    
+    func testSetHTTPHeaderFields() {
+        let url = URL(string: "https://www.google.com")
+        var urlRequest = URLRequest(url: url!)
+        let HTTPHeaderFields = ["Content-Type": "application/x-www-form-urlencoded; charset=utf-8", "Content-Length": "1"]
+        urlRequest.setHTTPHeaderFields(HTTPHeaderFields)
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Type"), "application/x-www-form-urlencoded; charset=utf-8")
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Length"), "1")
+    }
+    
+}
