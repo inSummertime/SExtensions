3f4ed73ed966a721c4571a57391d72aa0ae15f80
Ray <therayfeng@gmail.com>
2018-08-27 20:23:16 +0800

    URLRequest+SetHTTPHeaderFields

---
+++ Sources/Foundation/URLRequest/URLRequest+SetHTTPHeaderFields.swift
@@ -0,0 +1,31 @@
+//
+//  URLRequest+SetHTTPHeaderFields.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/26.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension URLRequest {
+    
+    /// Sets the HTTP header fields with a dictionary.
+    ///
+    ///     let url = URL(string: "https://www.google.com")
+    ///     var urlRequest = URLRequest(url: url!)
+    ///     let httpHeaderFields = ["Content-Type": "application/x-www-form-urlencoded; charset=utf-8"]
+    ///     urlRequest.setHTTPHeaderFields(httpHeaderFields)
+    ///     print(urlRequest.value(forHTTPHeaderField: "Content-Type") == "application/x-www-form-urlencoded; charset=utf-8")
+    ///     // Prints "true"
+    ///
+    /// - Parameter fields: A dictionary containing the HTTP header fields.
+    mutating func setHTTPHeaderFields(_ fields: [String: String]) {
+        for (key, value) in fields {
+            if value != "" {
+                setValue(value, forHTTPHeaderField: key)
+            }
+        }
+    }
+
+}
