aadf33e8a7556c31334f5b54376b1ad36b7f8752
Ray <therayfeng@gmail.com>
2018-08-28 07:28:30 +0800

    Bundle+Info

---
+++ Sources/Foundation/Bundle/Bundle+Info.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle+Info.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/8/28.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Bundle {
+    
+    var version: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    
+    var build: String? {
+        return infoDictionary?[kCFBundleVersionKey as String] as? String
+    }
+    
+    var appName: String? {
+        return infoDictionary?[kCFBundleNameKey as String] as? String
+    }
+    
+    var bundleID: String? {
+        return infoDictionary?[kCFBundleIdentifierKey as String] as? String
+    }
+    
+}
