1ffcc69afb14ebbf433ad7ee2e329618f6d92448
Ray <therayfeng@gmail.com>
2018-09-01 11:43:34 +0800

    JSONSerializationStringTests

---
+++ Tests/Foundation/JSONSerialization/JSONSerializationStringTests.swift
@@ -0,0 +1,25 @@
+//
+//  JSONSerializationStringTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/1.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class JSONSerializationStringTests: XCTestCase {
+    
+    func testJSONStringWithDictionary() {
+        let dictionary: [String: Any] = ["id": 0, "title": "title"]
+        XCTAssertEqual(JSONSerialization.jsonString(with: dictionary)!, "{\"id\":0,\"title\":\"title\"}")
+    }
+    
+    func testJSONStringWithArray() {
+        let array: [[String: Any]] = [["id": 0], ["title": "title"]]
+        XCTAssertEqual(JSONSerialization.jsonString(with: array)!, "[{\"id\":0},{\"title\":\"title\"}]")
+        print(JSONSerialization.jsonString(with: array)!)
+    }
+    
+}
