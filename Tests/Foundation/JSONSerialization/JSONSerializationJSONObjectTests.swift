fdc83af2a407d18c3e30f056790fc8df01a4fe7a
Ray <therayfeng@gmail.com>
2018-09-03 07:31:33 +0800

    JSONSerializationJSONObjectTests

---
+++ Tests/Foundation/JSONSerialization/JSONSerializationJSONObjectTests.swift
@@ -0,0 +1,37 @@
+//
+//  JSONSerializationJSONObjectTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/3.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class JSONSerializationJSONObjectTests: XCTestCase {
+    
+    func testJSONObjectFromFile() {
+        do {
+            let object = try JSONSerialization.jsonObjectFromFile(filename: "Test", callerClass: JSONSerializationJSONObjectTests.self)
+            let anotherObject = try  JSONSerialization.jsonObjectFromFile(filename: "Test.json", callerClass: JSONSerializationJSONObjectTests.self)
+            XCTAssertEqual((object! as! [String: Any])["id"] as! Int, 0)
+            XCTAssertEqual((anotherObject! as! [String: Any])["title"] as! String, "title")
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    func testDictionaryFromJSONString() {
+        let string = "{\"ids\":[\"1\",\"2\"],\"title\":[\"title\"]}"
+        do {
+            let object = try JSONSerialization.jsonObjectFromString(string)
+            XCTAssertEqual((object! as! [String: Any])["ids"] as! [String], ["1", "2"])
+        } catch {
+            XCTFail()
+        }
+    }
+    
+    
+
+}
