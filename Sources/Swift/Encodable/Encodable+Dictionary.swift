a79c9c160473cabe7883853ebc7526cb6e63225e
Ray <therayfeng@gmail.com>
2018-09-06 07:04:32 +0800

    Encodable+Dictionary

---
+++ Sources/Swift/Encodable/Encodable+Dictionary.swift
@@ -0,0 +1,21 @@
+//
+//  Encodable+Dictionary.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/6.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Encodable {
+    
+    /// Create a dictionary from a object which confirms Encodable
+    var dictionary: [String: Any]? {
+        guard let data = try? JSONEncoder().encode(self) else { return nil }
+        let jsonObject = try? JSONSerialization.jsonObject(with: data, options: .allowFragments)
+        let dictionary = jsonObject.flatMap { $0 as? [String: Any] }
+        return dictionary
+    }
+    
+}
