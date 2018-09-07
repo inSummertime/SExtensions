1f9c4a1a456e4a3c1cee7f97cd8333fa30a40b81
Ray <therayfeng@gmail.com>
2018-09-07 08:45:13 +0800

    DispatchQueue+Delay

---
+++ Sources/Dispatch/DispatchQueue+Delay.swift
@@ -0,0 +1,22 @@
+//
+//  DispatchQueue+Delay.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/7.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+public extension DispatchQueue {
+    
+    /// Performs the action after a specified seconds.
+    ///
+    /// - Parameter seconds: A specified time in seconds.
+    /// - Parameter completion: The completion handler.
+    func delay(_ seconds: Double, completionHandler: @escaping ()->()) {
+        let delayTime = DispatchTime.now() + Double(Int64(seconds * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        asyncAfter(deadline: delayTime) {
+            completionHandler()
+        }
+    }
+    
+}
