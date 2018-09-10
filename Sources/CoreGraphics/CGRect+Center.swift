f25b6ee9fd8bf8da0bd70421662d7940f4c56933
Ray <therayfeng@gmail.com>
2018-09-11 07:10:23 +0800

    CGRect+Center

---
+++ Sources/CoreGraphics/CGRect+Center.swift
@@ -0,0 +1,22 @@
+//
+//  CGRect+Center.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/11.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension CGRect {
+    
+    /// The center CGPoint.
+    ///
+    ///     let rect = CGRect(x: 0, y: 0, width: 2.0, height: 4.0)
+    ///     print(rect.center == CGPoint(x: 1.0, y: 2.0))
+    ///     // Prints "true"
+    var center: CGPoint {
+        return CGPoint(x: midX, y: midY)
+    }
+    
+}
