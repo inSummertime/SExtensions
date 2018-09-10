2f376f0a5c3316bb789602063021ddf61475706a
Ray <therayfeng@gmail.com>
2018-09-11 07:10:34 +0800

    CGRectCenterTests

---
+++ Tests/CoreGraphics/CGRectCenterTests.swift
@@ -0,0 +1,19 @@
+//
+//  CGRectCenterTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/11.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CGRectCenterTests: XCTestCase {
+    
+    func testCenter() {
+        let rect = CGRect(x: 0, y: 0, width: 2.0, height: 4.0)
+        XCTAssertEqual(rect.center, CGPoint(x: 1.0, y: 2.0))
+    }
+    
+}
