49bebe40f6a464d37435e95483f3491d86924f08
Ray <therayfeng@gmail.com>
2018-09-13 07:59:16 +0800

    Int+Padded

---
+++ Sources/Swift/Int/Int+Padded.swift
@@ -0,0 +1,24 @@
+//
+//  Int+Padded.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/13.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Int {
+    
+    /// Returns a padded string with the given field width.
+    ///
+    ///     print(1.padded(fieldWidth: 10) == "0000000001")
+    ///     // Prints "true"
+    ///
+    /// - Parameter fieldWidth: Field width.
+    /// - Returns: A string.
+    func padded(fieldWidth: Int) -> String {
+        return String(format: "%0*d", fieldWidth, self)
+    }
+    
+}
