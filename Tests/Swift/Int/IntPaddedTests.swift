7f3df9b3ae542d04c6cca40d2917c15ab065f2d5
Ray <therayfeng@gmail.com>
2018-09-13 07:59:25 +0800

    IntPaddedTests

---
+++ Tests/Swift/Int/IntPaddedTests.swift
@@ -0,0 +1,20 @@
+//
+//  IntPaddedTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/13.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class IntPaddedTests: XCTestCase {
+    
+    func testPadded() {
+        XCTAssertEqual(1.padded(fieldWidth: 10), "0000000001")
+        XCTAssertEqual((-1).padded(fieldWidth: 2), "-1")
+        XCTAssertEqual((-1).padded(fieldWidth: 3), "-01")
+    }
+    
+}
