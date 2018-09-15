481f53aabc6c5a3832410158ca3e918faf72f97e
Ray <therayfeng@gmail.com>
2018-09-15 11:24:29 +0800

    String+Is

---
+++ Sources/Swift/String/String+Is.swift
@@ -0,0 +1,41 @@
+//
+//  String+Is.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/14.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension String {
+    
+    /// Checks whether it is a phone number.
+    ///
+    ///     print("800–692–7753".isPhoneNumber)
+    ///     // Prints "true"
+    var isPhoneNumber: Bool {
+        do {
+            let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.phoneNumber.rawValue)
+            let matches = detector.matches(in: self, options: [], range: NSMakeRange(0, count))
+            if let first = matches.first {
+                return first.resultType == .phoneNumber && first.range.location == 0 && first.range.length == count
+            } else {
+                return false
+            }
+        } catch {
+            return false
+        }
+    }
+    
+    /// Checks whether it is an email.
+    ///
+    ///     print("123@a.com".isEmail)
+    ///     // Prints "true"
+    var isEmail: Bool {
+        let regularExpression = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regularExpression)
+        return predicate.evaluate(with: self)
+    }
+    
+}
