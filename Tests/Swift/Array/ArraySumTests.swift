6fd77af8d18db22d120cd4b82069bb6e349a4e8a
Ray <therayfeng@gmail.com>
2018-09-18 09:02:24 +0800

    ArraySumTests

---
+++ Tests/Swift/Array/ArraySumTests.swift
@@ -0,0 +1,20 @@
+//
+//  ArraySumTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class ArraySumTests: XCTestCase {
+    
+    func testSum() {
+        XCTAssertEqual([1, 2, 3, 4].sum, 10)
+        XCTAssertEqual([1.1, 2.2, 3.3, 4.4].sum, 11.0)
+        XCTAssertEqual(["Hello", " ", "world", "!"].sum, "Hello world!")
+    }
+    
+}
