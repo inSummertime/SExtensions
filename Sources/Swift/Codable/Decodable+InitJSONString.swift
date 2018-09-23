7e1e896a9b444e311d1396826afd8a32a9ee8a44
Ray <therayfeng@gmail.com>
2018-09-23 08:00:54 +0800

    Decodable+InitJSONString

---
+++ Sources/Swift/Codable/Decodable+InitJSONString.swift
@@ -0,0 +1,23 @@
+//
+//  Decodable+InitJSONString.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/23.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Decodable {
+    
+    /// Init using a JSON string.
+    ///
+    /// - Parameter jsonString: A JSON String
+    /// - Throws: throws an error when failing to create a Foundation object
+    ///   from JSON data.
+    init?(jsonString: String) throws {
+        guard let jsonData = jsonString.data(using: .utf8) else { return nil }
+        self = try JSONDecoder().decode(Self.self, from: jsonData)
+    }
+    
+}
