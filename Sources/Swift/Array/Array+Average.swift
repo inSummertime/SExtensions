2114bf9330fd22aa558823eda741a9ec409392ad
Ray <therayfeng@gmail.com>
2018-09-24 09:15:14 +0800

    Array+Average

---
+++ Sources/Swift/Array/Array+Average.swift
@@ -0,0 +1,31 @@
+//
+//  Array+Average.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/24.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+public extension Array where Element: Numeric {
+    
+    /// Returns the sum of all elements.
+    var total: Element { return reduce(0, +) }
+}
+
+public extension Array where Element: BinaryInteger {
+    
+    /// Returns the average of all elements.
+    var average: Double {
+        return isEmpty ? 0 : Double(total) / Double(count)
+    }
+    
+}
+
+extension Array where Element: BinaryFloatingPoint {
+    
+    /// Returns the average of all elements.
+    var average: Element {
+        return isEmpty ? 0 : total / Element(count)
+    }
+    
+}
