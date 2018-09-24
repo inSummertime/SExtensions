5fba4eda01c44c0d6f983b1d50d86d45eca1c43a
Ray <therayfeng@gmail.com>
2018-09-24 09:15:24 +0800

    ArrayAverageTests

---
+++ Tests/Swift/Array/ArrayAverageTests.swift
@@ -0,0 +1,23 @@
+//
+//  ArrayAverageTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/9/24.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class ArrayAverageTests: XCTestCase {
+    
+    func testAverage() {
+        XCTAssertEqual([0, 1, 2].average, 1.0)
+        XCTAssertEqual([0b0, 0b1, 0b10].average, 1.0)
+        XCTAssertEqual([0o0, 0o1, 0o2].average, 1.0)
+        XCTAssertEqual([0x0, 0x1, 0x2].average, 1.0)
+        XCTAssertEqual([0.0, 1.1, 2.2].average, 1.1)
+        XCTAssertEqual([Float(0), Float(1.1), Float(2.2)].average, 1.1)
+    }
+
+}
