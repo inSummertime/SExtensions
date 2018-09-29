2969aca15d82c5f4d222581280f11b1a797538ba
Ray <therayfeng@gmail.com>
2018-09-29 20:04:45 +0800

    Optional+IsNilOrEmpty

---
+++ Sources/Swift/Optional/Optional+IsNilOrEmpty.swift
@@ -0,0 +1,18 @@
+//
+//  Optional+IsNilOrEmpty.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/9/29.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+public extension Optional where Wrapped: Collection {
+    
+    /// Returns true if it is nil or empty.
+    var isNilOrEmpty: Bool {
+        guard let collection = self else { return true }
+        return collection.isEmpty
+    }
+    
+}
+
