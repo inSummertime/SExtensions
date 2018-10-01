60fc87aba796f91326062d4a962b181dafaade64
Ray <therayfeng@gmail.com>
2018-10-01 08:45:03 +0800

    ArrayIndexedDictionaryTests

---
+++ Tests/Swift/Array/ArrayIndexedDictionaryTests.swift
@@ -0,0 +1,21 @@
+//
+//  ArrayIndexedDictionaryTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/10/1.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class ArrayIndexedDictionaryTests: XCTestCase {
+
+    func testIndexedDictionary() {
+        let dictionary: [Int: Any] = ["hello", "world", "!", 0].indexedDictionary
+        XCTAssertEqual(dictionary[0] as! String, "hello")
+        XCTAssertEqual(dictionary[3] as! Int, 0)
+        XCTAssertEqual([Double]().indexedDictionary, [:])
+    }
+
+}
