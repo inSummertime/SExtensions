534b63d9be2084c296eef63e1692db4b7305946f
Ray <therayfeng@gmail.com>
2018-10-05 07:13:42 +0800

    Set+Average

---
+++ Sources/Swift/Set/Set+Average.swift
@@ -0,0 +1,33 @@
+//
+//  Set+Average.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/10/5.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Set where Element: Numeric {
+    
+    /// Returns the sum of all elements.
+    var total: Element { return reduce(0, +) }
+}
+
+public extension Set where Element: BinaryInteger {
+    
+    /// Returns the average of all elements.
+    var average: Double {
+        return isEmpty ? 0 : Double(total) / Double(count)
+    }
+    
+}
+
+extension Set where Element: BinaryFloatingPoint {
+    
+    /// Returns the average of all elements.
+    var average: Element {
+        return isEmpty ? 0 : total / Element(count)
+    }
+    
+}
--- Tests/Swift/Array/ArrayAverageTests.swift
+++ Tests/Swift/Array/ArrayAverageTests.swift
@@ -12,12 +12,12 @@ import XCTest
 final class ArrayAverageTests: XCTestCase {
     
     func testAverage() {
-        XCTAssertEqual([0, 1, 2].average, 1.0)
+        XCTAssertEqual([0, 1, 2, 1].average, 1.0)
         XCTAssertEqual([0b0, 0b1, 0b10].average, 1.0)
         XCTAssertEqual([0o0, 0o1, 0o2].average, 1.0)
         XCTAssertEqual([0x0, 0x1, 0x2].average, 1.0)
-        XCTAssertEqual([0.0, 1.1, 2.2].average, 1.1)
-        XCTAssertEqual([Float(0), Float(1.1), Float(2.2)].average, 1.1)
+        XCTAssertEqual([0.0, 1.1, 2.2, 1.1].average, 1.1)
+        XCTAssertEqual([Float(0), Float(1.1), Float(2.2), Float(1.1)].average, 1.1)
     }
 
 }
