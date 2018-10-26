5e592604c1642563299ba9e8840e98b52b0d0ce4
Ray <therayfeng@gmail.com>
2018-10-26 22:31:07 +0800

    Character+Case

---
+++ Sources/Swift/Character/Character+Case.swift
@@ -0,0 +1,29 @@
+//
+//  Character+Case.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/10/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+public extension Character {
+    
+    /// Returns true if it is uppercase
+    ///
+    ///     print(Character("A").isUppercase)
+    ///     // Prints "true"
+    var isUppercase: Bool {
+        return String(self).isUppercase
+    }
+    
+    /// Returns true if it is lowercase
+    ///
+    ///     print(Character("a").isLowercase)
+    ///     // Prints "true"
+    var isLowercase: Bool {
+        return String(self).isLowercase
+    }
+    
+}
