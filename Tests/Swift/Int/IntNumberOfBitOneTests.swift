44e4b8ba4ad65c306088c4c134ec24db4d6b2244
Ray <therayfeng@gmail.com>
2018-10-27 21:41:19 +0800

    IntNumberOfBitOneTests

---
+++ Tests/Swift/Int/IntNumberOfBitOneTests.swift
@@ -0,0 +1,30 @@
+//
+//  IntNumberOfBitOneTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/10/18.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+
+final class IntNumberOfBitOneTests: XCTestCase {
+
+    func testNumberOfBitOne() {
+        XCTAssertEqual(0.numberOfBitOne, 0)
+        XCTAssertEqual(1.numberOfBitOne, 1)
+        XCTAssertEqual(2.numberOfBitOne, 1)
+        XCTAssertEqual(3.numberOfBitOne, 2)
+        XCTAssertEqual((-4).numberOfBitOne, 1)
+    }
+    
+    func testNumberOfBitZero() {
+        XCTAssertEqual(0.numberOfBitZero, 1)
+        XCTAssertEqual(1.numberOfBitZero, 0)
+        XCTAssertEqual(2.numberOfBitZero, 1)
+        XCTAssertEqual(3.numberOfBitZero, 0)
+        XCTAssertEqual((-4).numberOfBitZero, 2)
+    }
+
+
+}
