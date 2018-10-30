08408e643e61731c1f650e1c9a2aecac32110957
Ray <therayfeng@gmail.com>
2018-10-30 12:15:57 +0800

    String+MemoryAddress

---
+++ Sources/Swift/String/String+MemoryAddress.swift
@@ -0,0 +1,23 @@
+//
+//  String+MemoryAddress.swift
+//  SExtensions
+//
+//  Created by Ray on 2018/10/30.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    /// Returns the memory address of an object.
+    ///
+    /// - Parameter object: AnyObject or nil.
+    /// - Returns: A String.
+    static func memoryAddress(of object: AnyObject?) -> String {
+        guard let object = object else { return "nil" }
+        let opaque: UnsafeMutableRawPointer = Unmanaged.passUnretained(object).toOpaque()
+        return String(describing: opaque)
+    }
+    
+}
