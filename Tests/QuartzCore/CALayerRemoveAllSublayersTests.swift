9100a43f62fe510a3a6b549c05f70df7ab81b2f7
Ray <therayfeng@gmail.com>
2018-11-02 08:21:22 +0800

    CALayerRemoveAllSublayersTests

---
+++ Tests/QuartzCore/CALayerRemoveAllSublayersTests.swift
@@ -0,0 +1,26 @@
+//
+//  CALayerRemoveAllSublayersTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/11/2.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CALayerRemoveAllSublayersTests: XCTestCase {
+
+    func test() {
+        let parentlayer = CALayer()
+        XCTAssertNil(parentlayer.sublayers)
+        let sublayer = CALayer()
+        parentlayer.addSublayer(sublayer)
+        let anotherSublayer = CALayer()
+        parentlayer.addSublayer(anotherSublayer)
+        XCTAssertTrue(parentlayer.sublayers!.count > 0)
+        parentlayer.removeAllSublayers()
+        XCTAssertNil(parentlayer.sublayers)
+    }
+
+}
