d6015581d1ee133ca64483f88d6414e8d9afa225
Ray <therayfeng@gmail.com>
2018-11-03 08:54:31 +0800

    CALayerAddColorGradientTests

---
+++ Tests/QuartzCore/CALayerAddColorGradientTests.swift
@@ -0,0 +1,39 @@
+//
+//  CALayerAddColorGradientTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2018/11/3.
+//  Copyright © 2018 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CALayerAddColorGradientTests: XCTestCase {
+
+    func testAddColorGradientHorizontal() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        view.layer.addColorGradient(startColor: .white, endColor: .black)
+        let gradientLayer = view.layer.sublayers?.first as? CAGradientLayer
+        XCTAssertNotNil(gradientLayer)
+        XCTAssertEqual(gradientLayer!.bounds, view.bounds)
+        XCTAssertEqual(gradientLayer!.colors?.first as! CGColor, UIColor.white.cgColor)
+        XCTAssertEqual(gradientLayer!.colors?.last as! CGColor, UIColor.black.cgColor)
+        XCTAssertEqual(gradientLayer!.startPoint.x, 0.0)
+        XCTAssertEqual(gradientLayer!.startPoint.y, 0.0)
+        XCTAssertEqual(gradientLayer!.endPoint.x, 1.0)
+        XCTAssertEqual(gradientLayer!.endPoint.y, 0.0)
+    }
+    
+    func testAddColorGradientVertical() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 10))
+        view.layer.addColorGradient(startColor: .white, endColor: .black, isHorizontal: false)
+        let gradientLayer = view.layer.sublayers?.first as? CAGradientLayer
+        XCTAssertNotNil(gradientLayer)
+        XCTAssertEqual(gradientLayer!.startPoint.x, 0.0)
+        XCTAssertEqual(gradientLayer!.startPoint.y, 0.0)
+        XCTAssertEqual(gradientLayer!.endPoint.x, 0.0)
+        XCTAssertEqual(gradientLayer!.endPoint.y, 1.0)
+    }
+
+}
