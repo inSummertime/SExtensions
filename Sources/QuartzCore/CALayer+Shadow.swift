384c7777a290ae3928a68ec7a097f2bb9811edb0
Ray <therayfeng@gmail.com>
2018-12-30 15:15:14 +0800

    Add addRoundShadow

--- Sources/QuartzCore/CALayer+Shadow.swift
+++ Sources/QuartzCore/CALayer+Shadow.swift
@@ -13,14 +13,27 @@ public extension CALayer {
     /// Adds a shadow.
     ///
     /// - Parameter color: The color of the shadow. Defaults to opaque black.
-    /// - Parameter offset: The shadow offset. Defaults to (0, -3).
     /// - Parameter radius: The blur radius used to create the shadow. Defaults to 3.
-    /// - Parameter opacity: The opacity of the shadow. Defaults to 0.
-    func addShadow(color: UIColor = UIColor.black, offset: CGSize = CGSize(width: 0, height: -3), radius: CGFloat = 3, opacity: Float = 0) {
+    /// - Parameter offset: The shadow offset. Defaults to (0, -3).
+    /// - Parameter opacity: The opacity of the shadow. Defaults to 0.5.
+    func addShadow(radius: CGFloat = 3, color: UIColor = UIColor.black, offset: CGSize = CGSize(width: 0, height: -3), opacity: Float = 0.5) {
+        shadowRadius = radius
         shadowColor = color.cgColor
         shadowOffset = offset
-        shadowRadius = radius
         shadowOpacity = opacity
+    }
+    
+    /// Adds a round shadow.
+    ///
+    /// - Parameters:
+    ///   - radius: The blur radius used to create the shadow. Defaults to 3.
+    ///   - color: The color of the shadow. Defaults to opaque black.
+    ///   - offset: The shadow offset. Defaults to (0, -3).
+    ///   - opacity: The opacity of the shadow. Defaults to 0.5.
+    func addRoundShadow(radius: CGFloat = 3, color: UIColor = UIColor.black, offset: CGSize = CGSize(width: 0, height: -3), opacity: Float = 0.5) {
+        addShadow(radius: radius, color: color, offset: offset, opacity: opacity)
+        cornerRadius = radius
+        shadowPath = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius).cgPath
     }
 
 }
