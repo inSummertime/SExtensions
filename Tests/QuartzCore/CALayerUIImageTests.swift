5b6136e979c4f83ebc2ba95ada861c0d0a4370b1
Ray <therayfeng@gmail.com>
2019-01-02 11:27:44 +0800

    CALayerUIImageTests

---
+++ Tests/QuartzCore/CALayerUIImageTests.swift
@@ -0,0 +1,23 @@
+//
+//  CALayerUIImageTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2019/1/2.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CALayerUIImageTests: XCTestCase {
+
+    func testImage() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        let subview = UIView(frame: CGRect(x: 16, y: 16, width: 32, height: 32))
+        subview.backgroundColor = .lightGray
+        view.addSubview(subview)
+        let image = view.layer.image
+        XCTAssertEqual(image!.size, CGSize(width: 64, height: 64))
+    }
+
+}
