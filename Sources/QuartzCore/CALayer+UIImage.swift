ca11c5ae425cc416eb7f4e095edf9366f1c9c745
Ray <therayfeng@gmail.com>
2019-01-02 11:27:33 +0800

    CALayer+UIImage

---
+++ Sources/QuartzCore/CALayer+UIImage.swift
@@ -0,0 +1,27 @@
+//
+//  CALayer+UIImage.swift
+//  SExtensions
+//
+//  Created by Ray on 2019/1/2.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import CoreGraphics
+import QuartzCore
+import UIKit
+
+public extension CALayer {
+    
+    var image: UIImage? {
+        defer {
+            UIGraphicsEndImageContext()
+        }
+        UIGraphicsBeginImageContextWithOptions(bounds.size, isOpaque, 0)
+        guard let context = UIGraphicsGetCurrentContext() else {
+            return nil
+        }
+        render(in: context)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+
+}
