fcd4207fd6680b74a215d8620c2e5d1e9fb3e1db
Ray <therayfeng@gmail.com>
2019-01-04 22:39:12 +0800

    CALayer+PDFData

---
+++ Sources/QuartzCore/CALayer+PDFData.swift
@@ -0,0 +1,23 @@
+//
+//  CALayer+PDFData.swift
+//  SExtensions
+//
+//  Created by Ray on 2019/1/4.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import QuartzCore
+
+public extension CALayer {
+    
+    var pdfData: NSData? {
+        let data = NSMutableData()
+        UIGraphicsBeginPDFContextToData(data, bounds, nil)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        UIGraphicsBeginPDFPage()
+        render(in: context)
+        UIGraphicsEndPDFContext()
+        return data
+    }
+    
+}
