b17f25d03aaf9d438a275bf6e1509e99a67a76a1
Ray <therayfeng@gmail.com>
2019-01-04 22:39:21 +0800

    CALayerPDFDataTests

---
+++ Tests/QuartzCore/CALayerPDFDataTests.swift
@@ -0,0 +1,39 @@
+//
+//  CALayerPDFDataTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2019/1/4.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import XCTest
+@testable import SExtensions
+
+final class CALayerPDFDataTests: XCTestCase {
+    
+    func testPDFData() {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        view.backgroundColor = .white
+        let subview = UIView(frame: CGRect(x: 16, y: 16, width: 32, height: 32))
+        subview.backgroundColor = .black
+        view.addSubview(subview)
+        guard let url = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).first else { return }
+        let URL = url.appendingPathComponent("view.pdf")
+        view.layer.pdfData?.write(to: URL, atomically: true)
+        do {
+            let data = try NSData(contentsOf: URL) as CFData
+            guard let provider = CGDataProvider(data: data) else {
+                XCTFail("Cannot create CGDataProvider.")
+                return
+            }
+            guard let pdfDoc = CGPDFDocument(provider) else {
+                XCTFail("Cannot create CGPDFDocument.")
+                return
+            }
+            XCTAssertEqual(pdfDoc.numberOfPages, 1)
+        } catch {
+            XCTFail("Cannot create NSData.")
+        }
+    }
+    
+}
