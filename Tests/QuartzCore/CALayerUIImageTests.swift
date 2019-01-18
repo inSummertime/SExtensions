a9c10ad7308daab52e1acc68286df75e57829d9c
Ray <therayfeng@gmail.com>
2019-01-18 09:42:53 +0800

    Update CALayerUIImageTests

--- Tests/QuartzCore/CALayerUIImageTests.swift
+++ Tests/QuartzCore/CALayerUIImageTests.swift
@@ -15,6 +15,7 @@ final class CALayerUIImageTests: XCTestCase {
         let view = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
         let subview = UIView(frame: CGRect(x: 16, y: 16, width: 32, height: 32))
         subview.backgroundColor = .lightGray
+        view.isOpaque = false
         view.addSubview(subview)
         let image = view.layer.image
         XCTAssertEqual(image?.size, CGSize(width: 64, height: 64))
