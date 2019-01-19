be3233e478d6569dbc346b6d1644e8ee9ce7cd02
Ray <therayfeng@gmail.com>
2019-01-19 11:11:08 +0800

    CGImageThumbnailTests

---
+++ Tests/CoreGraphics/CGImageThumbnailTests.swift
@@ -0,0 +1,25 @@
+//
+//  CGImageThumbnailTests.swift
+//  SExtensionsTests
+//
+//  Created by Ray on 2019/1/19.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+@testable import SExtensions
+import XCTest
+
+final class CGImageThumbnailTests: XCTestCase {
+
+    func testSourceCreate() {
+        let bundle = Bundle(for: type(of: self))
+        guard let url = bundle.url(forResource: "icon", withExtension: "png") else { return }
+        guard let cgImage = CGImage.createThumbnailWithURL(url, size: CGSize(width: 22, height: 26)) else {
+            XCTFail("Failed to create the CGImage")
+            return
+        }
+        XCTAssertEqual(cgImage.width, 22)
+        XCTAssertEqual(cgImage.height, 26)
+    }
+
+}
