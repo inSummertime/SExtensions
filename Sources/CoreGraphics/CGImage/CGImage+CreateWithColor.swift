be7999b4c8e988cd31377c25379d42840e3cb9ac
Ray <therayfeng@gmail.com>
2019-01-28 11:34:08 +0800

    CGImage+CreateWithColor

---
+++ Sources/CoreGraphics/CGImage/CGImage+CreateWithColor.swift
@@ -0,0 +1,29 @@
+//
+//  CGImage+CreateWithColor.swift
+//  SExtensions
+//
+//  Created by Ray on 2019/1/28.
+//  Copyright © 2019 Ray. All rights reserved.
+//
+
+import CoreGraphics
+import UIKit
+
+public extension CGImage {
+
+    /// Returns a CGImage with given a color and size.
+    ///
+    /// - Parameters:
+    ///   - color: A UIColor.
+    ///   - size: The Image size.
+    /// - Returns: Returns a CGImage if succeeds, otherwise returns nil.
+    class func createWithColor(_ color: UIColor, size: CGSize) -> CGImage? {
+        let layer = CALayer()
+        layer.backgroundColor = color.cgColor
+        layer.frame = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        UIGraphicsBeginImageContext(size)
+        guard let context = UIGraphicsGetCurrentContext() else { return nil }
+        layer.render(in: context)
+        return context.makeImage()
+    }
+}
